28d4e01fff6f7411fa56b3457b5b2811d9a0adf5
Janaki Keerthi <janakikeerthi277@gmail.com>
2021-09-04 20:20:43 +0530

    find post by id

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,6 +4,7 @@ import com.pluralsight.blog.data.PostRepository;
 import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,9 +21,15 @@ public class BlogController {
     @RequestMapping("/")
     public String listPosts(ModelMap map) {
         List<Post> posts = postRepository.getAllPosts();
-        map.put("title", "Blog Post 1");
         map.put("posts", posts);
 
         return "home";
+    }
+
+    @RequestMapping("/post/{id}")
+    public String postDetails(@PathVariable Long id, ModelMap map){
+        Post post = postRepository.findById(id);
+        map.put("post", post);
+        return "post-details";
     }
 }
