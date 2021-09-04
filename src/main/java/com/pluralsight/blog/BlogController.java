800fa21f66114c568328f2b0499435b5c4faf86c
Janaki Keerthi <janakikeerthi277@gmail.com>
2021-09-04 19:19:48 +0530

    fetching posts implemented

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,28 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
 
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository repository){
+        postRepository = repository;
+    }
+
     @RequestMapping("/")
-    public String listPosts( ModelMap map) {
+    public String listPosts(ModelMap map) {
+        List<Post> posts = postRepository.getAllPosts();
         map.put("title", "Blog Post 1");
+        map.put("posts", posts);
+
         return "home";
     }
 }
